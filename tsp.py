044a319e1993ff1779f3333a695cf193a6134e6a
lucas <lucas.machado.gutierrez@gmail.com>
2019-07-06 04:14:07 -0300

    as-fixed

--- tsp.py
+++ tsp.py
@@ -1,5 +1,8 @@
 from random import randint
+from random import random
+from random import randrange
 from copy import copy
+import math
 
 class Graph:
 
@@ -52,7 +55,6 @@ class Graph:
 
         self.directed = bool(int(dat[0]))
         dat.pop(0)
-        print(dat)
 
         for line in dat:
             l = line.split(" ")
@@ -119,6 +121,7 @@ class Graph:
         for i in range(len(vertices)-1):
             size += self.getWeight(vertices[i], vertices[i+1])
 
+        #print(vertices[0], vertices[len(vertices)-1])
         size += self.getWeight(vertices[0], vertices[len(vertices)-1])
 
         return size
@@ -147,20 +150,36 @@ class Graph:
 
 class Ant:
 
-    def __init__(self, startnode, G):
-        self.restart(startnode)
+    def __init__(self, G):
         self.G = G
+        self.restart()
 
-    def restart(self, startnode):
-        self.startnode = startnode
-        self.visited = [startnode]
-        self.currentnode = startnode
-        self.pathsize = 0
+    def restart(self, G = None):
+        if G != None:
+            self.G = G
+
+        V = self.G.getVertices()
+        self.startnode = V[randrange(len(V))] 
+        self.visited = [self.startnode]
+        self.currentnode = self.startnode
 
     def update(self, newnode):
-        self.pathsize += G.getWeight(self.currentnode, newnode)
         self.currentnode = newnode
         self.visited.append(newnode)
+
+    def getpathsize(self):
+        return G.pathSize(self.visited)
+
+    def getedges(self):
+        edges = []
+
+        for i in range(1, len(self.visited)):
+            edges.append((self.visited[i-1], self.visited[i]))
+
+        edges.append((self.visited[i], self.visited[0]))
+
+        return edges
+
 
 class PheromoneTable:
 
@@ -170,9 +189,9 @@ class PheromoneTable:
         for u in nodes:
             for v in nodes:
                 if u != v and self.table.get((u,v)) == None and self.table.get((v,u)) == None:
-                    self.table[(u,v)] = 0
+                    self.table[(u,v)] = 0.01
 
-    def adjust(edge):
+    def adjust(self, edge):
         u, v = edge
         if self.table.get((u,v)) == None:
             v, u = edge
@@ -180,28 +199,97 @@ class PheromoneTable:
         return (u,v)
 
     def get(self, edge):
-        u, v = adjust(edge)
+        u, v = self.adjust(edge)
 
         return self.table[(u,v)]
 
+    def evaporation(self, p):
+        for edge in self.table:
+            self.table[edge] *= (1-p)
+
     def update(self, ants):
-        pass
+        for ant in ants:
+            edges = ant.getedges()
+            L = ant.getpathsize()
+            for edge in edges:
+                u, v = self.adjust(edge)
+                self.table[(u,v)] += (1 / L)
 
 def ACO_AS(G, m, epochs, alfa = 1, beta = 5, p = 0.5):
     V = G.getVertices()
     n = len(V)
     pheromonetable = PheromoneTable(V)
+    globalbest = math.inf
 
     ants = []
     for i in range(m):
-        ants.append(Ant(randlist(V)))
+        ants.append(Ant(G))
 
     while (epochs > 0):
-        pass
 
-def randlist(l):
-    return l[randint(0, len(l) - 1)]
+        localbest = antactivity(ants, alfa, beta, pheromonetable)
+        pheromonetable.evaporation(p)
+        pheromonetable.update(ants)
 
-def probaction(ant, dest, pheromonetable, G, alfa, beta):
-    pass
+        for ant in ants:
+            ant.restart()
+
+        if localbest < globalbest:
+            globalbest = localbest
+
+        epochs -= 1
+
+    return globalbest
+
+def antactivity(ants, alfa, beta, pheromonetable):
+    m = len(ants)
+    G = ants[0].G
+    V = G.getVertices()
+    n = len(V)
+
+    for i in range(n):
+        for ant in ants:
+            adj = list(set(V).difference(set(ant.visited)))
+            prob = []
+            p = random()
+
+            for v in adj:
+                prob.append(probaction((ant.currentnode, v), pheromonetable, G, alfa, beta))
+
+            s = sum(prob)
+            for j in range(len(prob)):
+                prob[j] /= s
+
+            for j in range(1, len(prob)):
+                prob[j] += prob[j-1]
+
+            for j in range(len(prob)):
+                if p < prob[j]:
+                    ant.update(adj[j])
+                    break
+
+    best = math.inf
+    for ant in ants:
+        pathsize = ant.getpathsize()
+        if pathsize < best:
+            best = pathsize
+
+    print(best)
+
+    return best
+
+def probaction(edge, pheromonetable, G, alfa, beta):
+    u, v = edge
+    t = pheromonetable.get((u,v))
+    n = 1 / G.getWeight(u,v)
+
+    return pow(t, alfa) * pow(n, beta)
+
+#G = Graph(numvertices = 50, completed = True, randweight = 1000)
+#G.save("graph")
+G = Graph(filename = "graph")
+path = G.minimalPath(0)
+print(path, G.pathSize(path))
+
+#print(ACO_AS(G, 100, 50, alfa = 5, beta = 5, p = 0.3))
 
